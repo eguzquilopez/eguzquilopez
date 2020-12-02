68349b423fbca6d243a32b3e4abd24d20bfd391b
eguzqui lopez <eguzqui.lopez@icloud.com>
2020-12-01 21:58:40 -0300

    update

---
+++ main/eguzquilopez/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer'
+import React from 'react';
+import './maincss.css'
+
+
+function App() {
+  return (
+    <Footer />
+  );
+}
+
+export default App;
---
+++ main/eguzquilopez/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// const textoFooter = <h6>Pagina creada por mi, Eguzqui Lopez como proyecto final en Senpai Academy</h6>
+
+function Footer (prop) {
+    return (
+        <footer>
+            <h6>Pagina creada por Eguzqui Lopez para proyecto final Senpai Academy - 2020</h6>
+        </footer>
+    )
+}
+
+export default Footer;
+
+// export {textoFooter};
